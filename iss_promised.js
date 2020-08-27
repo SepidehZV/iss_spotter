0fc4af40bfe19bc9c199aa7576fc2ec4e45fa177
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-27 15:07:13 +0000

    ISS with chain of promises

---
+++ iss_promised.js
@@ -0,0 +1,47 @@
+// iss_promised.js
+const request = require('request-promise-native');
+
+/*
+ * Requests user's ip address from https://www.ipify.org/
+ * Input: None
+ * Returns: Promise of request for ip data, returned as JSON string
+ */
+const fetchMyIP = function() {
+  return request('https://api.ipify.org?format=json');
+};
+
+/* Makes a request to ipvigilante.com using the provided IP address, to get its geographical information (latitude/longitude)
+ * Input: JSON string containing the IP address
+ * Returns: Promise of request for lat/lon
+ */
+const fetchCoordsByIP = function(body) {
+  const ip = JSON.parse(body).ip;
+  return request(`https://ipvigilante.com/${ip}`)
+};
+
+const fetchISSFlyOverTimes = function(body) {
+  const latitude = JSON.parse(body).data.latitude;
+  const longitude = JSON.parse(body).data.longitude;
+  return request(`http://api.open-notify.org/iss-pass.json?lat=${latitude}&lon=${longitude}`);
+};
+
+const nextISSTimesForMyLocation = function (body) {
+  return fetchMyIP()
+    .then(fetchCoordsByIP)
+    .then(fetchISSFlyOverTimes)
+    .then((body) => {
+      const { response } = JSON.parse(body);
+      return response;
+    });
+};
+
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
+
+module.exports = { nextISSTimesForMyLocation, printPassTimes };
